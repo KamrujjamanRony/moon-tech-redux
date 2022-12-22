435d6fb0c4c8720070c65508ca601e801d4ec6a4
KamrujjamanRony <kamrujjaman0228@gmail.com>
2022-12-22 07:36:12 +0600

    active class

--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -10,15 +10,33 @@ export const initialState = {
 const filterReducer = (state = initialState, action) => {
     switch(action.type){
       case TOGGLE_BRAND: 
+      if(!state.filters.brands.includes(action.payload)){
         return {
-            ...state,
-            filters: {
-                ...state.filters,
-                brands: [...state.filters.brands, action.payload],
-            },
+          ...state,
+          filters: {
+            ...state.filters,
+            brands: [...state.filters.brands, action.payload],
+          },
         };
+      } 
+      else {
+        return {
+          ...state,
+          filters: {
+            ...state.filters,
+            brands: state.filters.brands.filter(brand=> brand !== action.payload),
+          },
+        };
+      } 
+      
         case TOGGLE_STOCK: 
-        return {};
+        return {
+          ...state,
+          filters: {
+            ...state.filters,
+            stock: !state.filters.stock,
+          },
+        };;
         default: 
         return state;
     }
