1caacf578c6b188cc32880c311e6d6a486caf123
KamrujjamanRony <kamrujjaman0228@gmail.com>
2022-12-24 00:32:59 +0600

    apply middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(rootReducer, composeWithDevTools());
+// composeWithDevTools()
+const store = createStore(rootReducer, );
 
 export default store;
