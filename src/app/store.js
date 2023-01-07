0cf52b37271d61193a4803e0fcaad75473dc335e
KamrujjamanRony <kamrujjaman0228@gmail.com>
2023-01-07 08:22:13 +0600

    cart and filter done

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "../features/counter/counterSlice";
+import cartSlice from "../features/cart/cartSlice";
+import filterSlice from "../features/filter/filterSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterSlice,
+    carts: cartSlice,
+    filters: filterSlice,
   },
 });
--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import { toggle, toggleBrands } from "../../features/filter/filterSlice";
+
 const Home = () => {
-  
+  const dispatch = useDispatch();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -39,17 +42,20 @@ const Home = () => {
   return (
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
-        <button 
+        <button
+          onClick={() => dispatch(toggle())}
           className={`border px-3 py-2 rounded-full font-semibold`}
         >
           In Stock
         </button>
         <button
+          onClick={() => dispatch(toggleBrands("amd"))}
           className={`border px-3 py-2 rounded-full font-semibold`}
         >
           AMD
         </button>
         <button
+          onClick={() => dispatch(toggleBrands("intel"))}
           className={`border px-3 py-2 rounded-full font-semibold`}
         >
           Intel
@@ -57,8 +63,8 @@ const Home = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
         {products.map((product) => (
-        <ProductCard key={product.model} product={product} />
-      ))}
+          <ProductCard key={product.model} product={product} />
+        ))}
       </div>
     </div>
   );
